ea4d2380dd358e3144987e32e5e32a1833db851c
l.difini <l.difini@mitric.com>
2024-07-31 15:13:41 +0200

    First commit

---
+++ Condition-TestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿// Dice rolling simulation -- Create decision logic with if statements
+Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"\nDice roll: {roll1} + {roll2} + {roll3} = {total} ");
+
+if (roll1 == roll2 && roll2 == roll3)
+{
+    total += 6;
+    Console.WriteLine("You rolled triples! +6 bonus to total! Currently total: " + total);
+}
+else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    total += 2;
+    Console.WriteLine("You rolled double! +2 bonus to total! Currently total: " + total);
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
---
+++ Condition-TestProject2/Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new();
+
+int daysUntilExpiration = random.Next(1, 12);
+int discountPercentage = 0; // Inizializzazione della variabile
+
+Console.WriteLine("Days to expiration : " + daysUntilExpiration);
+
+if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day! Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
