db2fa8eab56f87b86f462488a17d3376aca0c863
Luca <luke.difini@gmail.com>
2024-09-24 01:35:01 +0200

    studentScores

--- CodeComment-TestProject4/obj/Debug/net8.0/CodeComment-TestProject4.AssemblyInfo.cs
+++ CodeComment-TestProject4/obj/Debug/net8.0/CodeComment-TestProject4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeComment-TestProject4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674dc097c717ab02e7079cc2532aa5756e6d486e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57133ef6e1c2655b0db26cf1c046e2298b2ee49")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeComment-TestProject4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeComment-TestProject4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
